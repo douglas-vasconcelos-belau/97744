1507c06e763081995d1aa4856bea1c969a44ca4c
douglas-vasconcelos-belau <douglasbelau2004@gmail.com>
2025-02-14 20:03:48 +0000

    salvando

---
+++ atividades/5.atividade.py
@@ -0,0 +1,14 @@
+import os
+
+os.system ("clear")
+
+senha_c = "qwe"
+ligin_C = "douglas"
+login = str (input("digite seu login:"))
+senha = str (input("digite sua senha:"))
+
+if login_c ==login and senha_c ==senha :
+    print ("bem vindo")
+else:
+    print("login ou senha invalido")
+
