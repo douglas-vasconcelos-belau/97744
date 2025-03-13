01cb40b270a887aa371beb3bdd27ce6ba3af9858
douglas-vasconcelos-belau <douglasbelau2004@gmail.com>
2025-03-13 19:51:08 +0000

    salvar

---
+++ 1.conceitos iniciais/5.laço de repetiçao.py
@@ -0,0 +1,14 @@
+import os
+import time
+
+#for i in range (1,10):  a contagem começa com 0 entao sempre coloque 1 a mais.
+#    print ("qwe")
+
+#for i in range (1,10):
+#    print (f"valor da variavel: {i}")
+
+#for i in range (1,10,2): o ultimo serve pra pular de 2 em 2 
+#    print (f"valor da variavel: {i}")
+#    time.sleep(1) espera 1 segundo
+
+#print ("acabou")
---
+++ atividades/13.atividade.py
@@ -0,0 +1,4 @@
+for i in range (100,121,2): 
+    print (f"valor da variavel: {i}")
+
+print ("acabou")
