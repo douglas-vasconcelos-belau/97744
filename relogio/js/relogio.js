19b11dae239cd4d592d0ebcc4144965a2d763d53
douglas-vasconcelos-belau <douglasbelau2004@gmail.com>
2025-10-03 17:13:01 +0000

    salvar

---
+++ relogio/js/relogio.js
@@ -0,0 +1,27 @@
+const horas = document.querySelector('#hora')
+const minutos = document.querySelector('#minutos')
+const segundos = document.querySelector('#segundos')
+
+setInterval(relogio,1000)
+
+function relogio(){
+    hoje = new Date()
+    hr = hoje.getHours()
+    min = hoje.getMinutes()
+    seg = hoje.getSeconds()
+
+    if(hr<10){
+        hr = '0'+hr
+    }
+    if(min<10){
+        min = '0'+min
+    }
+    if(seg<10){
+        seg = '0'+seg
+    }
+
+    horas.textContent = hr
+    minutos.textContent = min
+    segundos.textContent = seg
+
+}
